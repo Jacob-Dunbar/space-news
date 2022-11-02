6da3ad9911b99ab63d4d3755e21537f8f9a4fd60
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-02 12:59:13 +0000

    feat: add astronaut comp with api props passed in

---
+++ components/Astronaut.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+function Astronaut(props) {
+  console.log(props);
+  return (
+    <div>
+      {/* <h3>{props.astronautName}</h3> */}
+      {/* <h4>{data}</h4> */}
+    </div>
+  );
+}
+
+export default Astronaut;
