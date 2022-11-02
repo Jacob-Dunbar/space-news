03436030ad2c28b0522e12c73d326ee5cf4c1c99
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-02 14:41:34 +0000

    feat: move fetch request into astronaut component

--- components/Craft.js
+++ components/Craft.js
@@ -1,38 +1,16 @@
 import Astronaut from "./Astronaut.js";
-import { useState, useEffect } from "react";
 
 function Craft(props) {
-  // const [data, setData] = useState(null);
-  // const [isLoading, setLoading] = useState(false);
+  // use craft data to generate astronaut components for each one
 
-  // fetch data for all astonauts on craft
-
-  // useEffect(() => {
-  //   setLoading(true);
-  //   fetch(
-  //     `https://ll.thespacedevs.com/2.2.0/astronaut/?search=${props.peopleOnBoard.toString()}`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setData(data);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
-  // if (isLoading) return <p>Loading...</p>;
-  // if (!data) return <p>No profile data</p>;
-
-  // use astronaut data to generate astronaut components for each one
-
-  // const astronautElements = data.map((person, index) => {
-  //   return <Astronaut key={index} astronaut={person} />;
-  // });
+  const astronautElements = props.peopleOnBoard.map((name, index) => {
+    return <Astronaut key={index} astronaut={name} />;
+  });
 
   return (
     <div>
       <h1>{props.craftName}</h1>
-      {/* {astronautElements} */}
-      {props.peopleOnBoard.toString()}
+      {astronautElements}
     </div>
   );
 }
