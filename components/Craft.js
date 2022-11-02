57e328dd9e59e7b375ac357820138dcc19be0997
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-02 13:00:16 +0000

    feat: use WIS data to fetch astronaut data

--- components/Craft.js
+++ components/Craft.js
@@ -1,14 +1,38 @@
-import React from "react";
+import Astronaut from "./Astronaut.js";
+import { useState, useEffect } from "react";
 
 function Craft(props) {
+  // const [data, setData] = useState(null);
+  // const [isLoading, setLoading] = useState(false);
+
+  // fetch data for all astonauts on craft
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch(
+  //     `https://ll.thespacedevs.com/2.2.0/astronaut/?search=${props.peopleOnBoard.toString()}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  // if (isLoading) return <p>Loading...</p>;
+  // if (!data) return <p>No profile data</p>;
+
+  // use astronaut data to generate astronaut components for each one
+
+  // const astronautElements = data.map((person, index) => {
+  //   return <Astronaut key={index} astronaut={person} />;
+  // });
+
   return (
     <div>
       <h1>{props.craftName}</h1>
-      <ul>
-        {props.peopleOnBoard.map((person, index) => (
-          <li key={index}>{person}</li>
-        ))}
-      </ul>
+      {/* {astronautElements} */}
+      {props.peopleOnBoard.toString()}
     </div>
   );
 }
