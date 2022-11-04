4df66a38ce74320c4dd804b2ec810b13979f776c
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-04 20:03:00 +0000

    feat: conditionally render crew list

--- components/Craft.js
+++ components/Craft.js
@@ -1,4 +1,5 @@
 import Astronaut from "./Astronaut.js";
+import { useState } from "react";
 
 function Craft(props) {
   // use craft data to generate astronaut components for each one
@@ -7,13 +8,31 @@ function Craft(props) {
     return <Astronaut key={index} astronaut={name} />;
   });
 
+  const [showPeople, setShowPeople] = useState(false);
+
+  console.log(astronautElements);
+
+  function handleBlur(e) {
+    // if the blur was because of outside focus
+    // currentTarget is the parent element, relatedTarget is the clicked element
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setShowPeople(false);
+    }
+  }
+
   return (
-    <div className="relative flex pt-2 pl-6 mb-4 border-l h-min w-max">
+    <div
+      onClick={() => props.changeCurrentCraft(props.craftName)}
+      onBlur={handleBlur}
+      className="relative flex pt-2 pl-6 mb-4 border-l h-min w-max"
+    >
       <div>
-        <h1 className="mb-3 text-4xl capitalize font-extralight text-slate-100">
+        <button className="mb-3 text-4xl text-left capitalize w-44 font-extralight text-slate-100">
           {props.craftName === "iss" ? "ISS" : props.craftName}
-        </h1>
-        {astronautElements}
+        </button>
+        {props.currentCraft === props.craftName ? (
+          <div>{astronautElements}</div>
+        ) : undefined}
       </div>
     </div>
   );
