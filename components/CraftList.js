ff797398766af3b7bb1d1bf7860f6f150836ab08
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-04 20:03:39 +0000

    feat: create craft list component

---
+++ components/CraftList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Craft from "../components/Craft";
+
+function CraftList(props) {
+  console.log(props.peopleInSpace);
+  const peopleInSpaceElements = props.peopleInSpace.map((craft, index) => {
+    return (
+      <Craft
+        key={index}
+        changeCurrentCraft={props.changeCurrentCraft}
+        craftName={craft.craft}
+        peopleOnBoard={craft.name}
+        currentCraft={props.currentCraft}
+      />
+    );
+  });
+
+  return <div>{peopleInSpaceElements}</div>;
+}
+
+export default CraftList;
