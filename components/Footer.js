f454f289abb4b242ff725b7f69d118b16e50dbed
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-06 11:08:39 +0000

    style: add footer basic styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="flex items-center justify-center h-10">
+      <h2 className="text-slate-200">Jacob Dunbar</h2>
+    </div>
+  );
 }
 
 export default Footer;
