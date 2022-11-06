b3ae4a8ca4fdd534031679f6d969f822217bf7cb
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-06 18:20:47 +0000

    feat: add name and website to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,16 @@
-import React from "react";
+import Link from "next/link";
 
 function Footer() {
   return (
-    <div className="flex items-center justify-center h-10">
-      <h2 className="text-slate-200">Jacob Dunbar</h2>
+    <div className="flex items-center justify-start h-10 gap-6">
+      <h2 className="pl-6 text-xs text-slate-600">Created by - Jacob Dunbar</h2>
+      <Link
+        className="text-xs text-orange-400 opacity-50 hover:opacity-80"
+        href={"http://jacobdunbar.com/"}
+      >
+        {" "}
+        www.jacobdunbar.com
+      </Link>
     </div>
   );
 }
