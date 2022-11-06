7ff8bca9f47019bf4a8768a152569cedd6e639e6
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-06 19:23:29 +0000

    style: change float intensity on floating text

--- components/Space-news.js
+++ components/Space-news.js
@@ -4,7 +4,7 @@ import { useGLTF, Float } from "@react-three/drei";
 export default function Model(props) {
   const { nodes, materials } = useGLTF("/models/text/space-news.gltf");
   return (
-    <Float speed={0.8} rotationIntensity={0.5} floatIntensity={4}>
+    <Float speed={0.6} rotationIntensity={0.5} floatIntensity={4}>
       <group
         rotation={[1.5, 0, 0]}
         scale={[2.5, 2.5, 2.5]}
