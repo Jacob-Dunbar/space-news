8aeac3faf1a52c189c230540f095f2185e1e9851
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-06 18:02:49 +0000

    chore: swap out 3d text for thiner font

--- components/Space-news.js
+++ components/Space-news.js
@@ -4,17 +4,17 @@ import { useGLTF, Float } from "@react-three/drei";
 export default function Model(props) {
   const { nodes, materials } = useGLTF("/models/text/space-news.gltf");
   return (
-    <Float speed={0.75} rotationIntensity={0.25} floatIntensity={4}>
+    <Float speed={0.8} rotationIntensity={0.5} floatIntensity={4}>
       <group
+        rotation={[1.5, 0, 0]}
+        scale={[2.5, 2.5, 2.5]}
         {...props}
         dispose={null}
-        rotation={[1.5, 0, 0]}
-        scale={[0.5, 0.5, 0.5]}
       >
         <mesh
-          geometry={nodes.Curve003.geometry}
-          material={materials["SVGMat.002"]}
-          scale={514.56}
+          geometry={nodes.Curve.geometry}
+          material={materials.SVGMat}
+          scale={80.35}
         />
       </group>
     </Float>
