464fcc9662fc8bd113f33e45b61489700356504b
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-06 20:02:22 +0000

    style: prelim navbar responsive design

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,16 @@
 import Link from "next/link";
 
 function Navbar(props) {
-  console.log(props.page);
   return (
-    <nav className=" w-full flex justify-between h-16 items-center w-100% px-5">
+    <nav className=" w-full flex  justify-between h-16 items-center w-100% sm:px-0 px-5">
       <div>
         <img
-          className="w-40 pt-2 pl-3"
+          className="w-40 pt-2 pl-3 sm:pl-5"
           src="/space-news-logo.svg"
           alt="space news logo"
         />
       </div>
-      <div className="flex">
+      <div className="flex sm:bg-black sm:fixed sm:justify-center sm:h-12 sm:items-center sm:gap-4 sm:w-full sm:z-20 sm:bottom-0">
         <Link
           onClick={() => props.changePage("home")}
           className={`px-3 text-xs font-bold tracking-widest uppercase cursor-pointer ${
@@ -23,7 +22,7 @@ function Navbar(props) {
         </Link>
         <Link
           onClick={() => props.changePage("feed")}
-          className={`px-3 text-xs font-bold tracking-widest uppercase cursor-pointer border-x ${
+          className={`px-3 sm:px-7  text-xs font-bold tracking-widest uppercase cursor-pointer border-x ${
             props.page === "feed" ? "text-orange-400" : "text-slate-300"
           }`}
           href="/feed/1"
@@ -32,7 +31,7 @@ function Navbar(props) {
         </Link>
         <Link
           onClick={() => props.changePage("isn")}
-          className={`px-3 text-xs font-bold tracking-widest uppercase cursor-pointer ${
+          className={`px-3 text-xs sm:w-24 text-center font-bold tracking-widest uppercase cursor-pointer ${
             props.page === "isn" ? "text-orange-400" : "text-slate-300"
           }`}
           href="/in-space-now"
--- components/Space-news.js
+++ components/Space-news.js
@@ -1,13 +1,38 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useGLTF, Float } from "@react-three/drei";
 
 export default function Model(props) {
   const { nodes, materials } = useGLTF("/models/text/space-news.gltf");
+
+  // Set state for if on mobile or desktop
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 415);
+
+  // Function to change state of isMobile
+  const updateIsMobile = () => {
+    setIsMobile(window.innerWidth < 415);
+    console.log(isMobile);
+  };
+
+  // Add event listener for screen resize and run updateIsMobile, plus cleanup.
+
+  useEffect(() => {
+    window.addEventListener("resize", updateIsMobile);
+    return () => window.removeEventListener("resize", updateIsMobile);
+  });
+
+  function choseScale() {
+    if (isMobile) {
+      return [1.2, 1.2, 1.2];
+    } else {
+      return [2.5, 2.5, 2.5];
+    }
+  }
+
   return (
     <Float speed={0.6} rotationIntensity={0.5} floatIntensity={4}>
       <group
         rotation={[1.5, 0, 0]}
-        scale={[2.5, 2.5, 2.5]}
+        scale={choseScale()}
         {...props}
         dispose={null}
       >
