fef4750113dc1ef32cb3995e264c3b1db0b3d2bf
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-06 19:21:13 +0000

    feat: add hover animation for link to article page

--- pages/feed/[slug]/article/[id]/index.js
+++ pages/feed/[slug]/article/[id]/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import Link from "next/link";
 
 function article({ articles }) {
   console.log(articles[0]);
@@ -22,10 +22,16 @@ function article({ articles }) {
         <h2 className="px-2 border-x text-slate-100">
           {articles[0].authorsByline}
         </h2>
-        <h2 className="px-2 text-slate-100">{articles[0].source.domain}</h2>
+        <Link
+          target="_blank"
+          href={articles[0].url}
+          className="px-2 text-orange-400 opacity-60 hover:opacity-90 text-slate-100"
+        >
+          {articles[0].source.domain}
+        </Link>
       </div>
       <img className="w-1/3 " src={articles[0].imageUrl} />
-      <p className="w-3/4 mb-10 text-slate-200 indent-8">
+      <p className="w-3/4 mb-10 leading-7 tracking-wide text-slate-400 indent-8">
         {articles[0].content}
       </p>
     </div>
