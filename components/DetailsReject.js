16abfb81bcf7ab879d3d70b07fe36ff44e164d87
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-07 13:21:08 +0000

    style: position details reject for mobile

--- components/DetailsReject.js
+++ components/DetailsReject.js
@@ -1,26 +1,34 @@
 import Link from "next/link";
-import { HiOutlineChevronDoubleRight } from "react-icons/hi";
+import {
+  HiOutlineChevronDoubleRight,
+  HiOutlineChevronDoubleLeft,
+} from "react-icons/hi";
 
 function DetailsReject(props) {
   return (
-    <div className="absolute top-0 flex flex-col pl-6 border-l mt-14 -right-60 h-min">
-      <div className="bg-red-500 h-fit"></div>
+    <div className="absolute top-0 flex flex-col pl-8 sm:bg-black border-l-[1px]  sm:h-full sm:w-64 mt-14 sm:z-20 sm:top-0 -right-64 sm:-right-12 h-min">
+      <p className="flex items-center hidden w-full gap-3 pb-4 mt-3 text-left text-slate-100 sm:inline">
+        <HiOutlineChevronDoubleLeft className="inline mr-2 " />
+        Back
+      </p>
 
-      <div className="w-44">
-        <h1 className="py-3 text-lg font-light border-b-0 text-slate-100 w-44">
-          {props.name}
-        </h1>
+      <div className="sm:flex sm:flex-col sm:justify-between sm:h-full w-44">
+        <div>
+          <h1 className="py-3 text-lg font-light border-b-0 text-slate-100 w-44">
+            {props.name}
+          </h1>
 
-        <p className="pt-4 text-sm font-light border-t w-44 indent-4 line-clamp-8 text-slate-100">
-          There is currently no data available for this astronaut.
-        </p>
+          <p className="pt-4 text-sm font-light border-t w-44 indent-4 line-clamp-8 text-slate-100">
+            There is currently no data available for this astronaut.
+          </p>
+        </div>
 
         <Link
-          className="flex items-center gap-3 mt-3 text-left text-slate-100 group hover:text-orange-400"
+          className="flex items-center gap-3 mt-3 text-left sm:justify-self-end text-slate-100 group hover:text-orange-400"
           target="_blank"
           href={`https://en.wikipedia.org/wiki/${props.name}`}
         >
-          Try Wikipedia{" "}
+          Try Wikipedia
           <HiOutlineChevronDoubleRight className=" group-hover:animate-bounceLeft" />
         </Link>
       </div>
