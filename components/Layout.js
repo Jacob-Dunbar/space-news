13030bc38394ef6a470edfc5c52c96d88df8d5ae
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-08 14:37:09 +0000

    feat: add current page highlight to navbar

--- components/Layout.js
+++ components/Layout.js
@@ -3,14 +3,9 @@ import Navbar from "../components/Navbar.js";
 import Footer from "../components/Footer.js";
 
 function Layout({ children }) {
-  const [currentPage, setCurrentPage] = useState("home");
-
-  function changeCurrentPage(page) {
-    setCurrentPage(page);
-  }
   return (
     <div className="relative flex flex-col justify-between h-full min-h-screen ">
-      <Navbar page={currentPage} changePage={changeCurrentPage} />
+      <Navbar />
       <div className="flex flex-col h-full grow ">{children}</div>
       <Footer />
       <div className="absolute w-full h-full bg-black -z-10 opacity-80"></div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -9,17 +9,6 @@ function Navbar(props) {
 
   // Render Link
 
-  <Link
-    href="/some-path"
-    className={
-      currentRoute === "/some-path"
-        ? "active-class-name"
-        : "non-active-class-name"
-    }
-  >
-    Some Link
-  </Link>;
-
   return (
     <nav className=" w-full flex  justify-between h-16 items-center w-100% sm:px-0 px-5">
       <div>
@@ -31,27 +20,28 @@ function Navbar(props) {
       </div>
       <div className="flex sm:bg-black sm:m-auto sm:left-0 sm:right-0 sm:fixed sm:justify-center sm:rounded-lg sm:h-12 sm:opacity-95 sm:items-center sm:bg-slate-900 sm:gap-4 sm:w-11/12 sm:z-30 sm:bottom-4">
         <Link
-          onClick={() => props.changePage("home")}
           className={`px-3 sm:px-2 text-xs font-bold tracking-widest uppercase cursor-pointer ${
-            props.page === "home" ? "text-orange-400" : "text-slate-300"
-          } `}
+            currentRoute === "/" ? " text-orange-400" : " text-slate-300"
+          }`}
           href="/"
         >
           Home
         </Link>
         <Link
-          onClick={() => props.changePage("feed")}
           className={`px-3 sm:px-6  text-xs font-bold tracking-widest uppercase cursor-pointer border-x ${
-            props.page === "feed" ? "text-orange-400" : "text-slate-300"
+            currentRoute === "/feed/[slug]"
+              ? " text-orange-400"
+              : " text-slate-300"
           }`}
           href="/feed/1"
         >
           News feed
         </Link>
         <Link
-          onClick={() => props.changePage("isn")}
           className={`px-3 sm:px-2 text-xs sm:w-24 text-center font-bold tracking-widest uppercase cursor-pointer ${
-            props.page === "isn" ? "text-orange-400" : "text-slate-300"
+            currentRoute === "/inSpaceNow"
+              ? " text-orange-400"
+              : " text-slate-300"
           }`}
           href="/inSpaceNow"
         >
