88b86d6c2db59a775ba0bc899cd123a103b933ae
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-12-13 19:35:41 +0000

    style: restyle article page mobile

--- pages/feed/[slug]/article/[id]/index.js
+++ pages/feed/[slug]/article/[id]/index.js
@@ -37,21 +37,21 @@ function article({ articles }) {
 
   return (
     // Container
-    <div className="flex flex-col items-center justify-center w-full h-full gap-8 mt-12">
+    <div className="flex flex-col items-center justify-center w-full h-full gap-8 mt-12 sm:mt-5">
       <Head>
         <title>Space News | Article</title>
         <meta name="Space News" content="Latest space news and information" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <div className="flex flex-col items-center mb-5 px-6 py-2  bg-gradient-to-bl from-black/10 to-white/10 w-[750px] border-2 border-gray-900 bg-opacity-5 backdrop-blur-sm rounded-xl">
+      <div className="flex flex-col items-center mb-5 px-6 py-2  bg-gradient-to-bl from-black/10 to-white/10 w-[750px] sm:w-[98%] border-2 border-gray-900 bg-opacity-5 backdrop-blur-sm rounded-xl">
         {/* Article title */}
-        <h1 className="my-5 text-2xl text-center text-slate-300">
+        <h1 className="my-5 text-2xl text-center sm:text-lg text-slate-300">
           {articles[0].title}
         </h1>
         {/* details section with date, author and source */}
-        <div className="flex mb-5 ">
+        <div className="flex mb-5 sm:mb-8 sm:items-center ">
           <h2 className="px-2 text-slate-300">{date}</h2>
-          <h2 className="px-2 border-x text-slate-300">
+          <h2 className="px-2 sm:px-2 sm:text-center border-x text-slate-300">
             {articles[0].authorsByline}
           </h2>
           {/* Link to source website */}
@@ -64,9 +64,9 @@ function article({ articles }) {
           </Link>
         </div>
         {/* Image */}
-        <img className="w-3/4 mb-10 " src={articles[0].imageUrl} />
+        <img className="w-3/4 mb-10 sm:w-full " src={articles[0].imageUrl} />
         {/* Article content */}
-        <p className="w-full px-5 mb-10 leading-7 tracking-wide text-slate-300 indent-8">
+        <p className="w-full px-5 mb-10 leading-7 tracking-wide sm:text-sm text-slate-300 indent-8">
           {articles[0].content}
         </p>
       </div>
